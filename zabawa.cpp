58164e5e1cc2fb38e08545074a8cbc17189ce3ee
Pbogdanski <32055904+Pbogdanski@users.noreply.github.com>
2021-12-16 23:17:37 +0100

    mam nadzieję że to faktycznie spowoduje konflikt
    dawno nie używałem gita

--- zabawa.cpp
+++ zabawa.cpp
@@ -3,5 +3,10 @@
 using namespace std;
 int main()
   {
-  cout << "Goodbye world!\n";
-  }
+  cout << "Hi world i'm mark!\n";
+// ale elegancki ten main tak nic sobie nie zwraca  
+for(int i =0; i<10000000;i++)
+{
+  cout<<"całe te"<<endl;   
+}
+}
